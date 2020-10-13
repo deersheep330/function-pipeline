02380632ced6478fc569d9beda98e2fe260a9192
YangChiu <yangchiu@inquartik.com>
2020-10-13 16:41:31 +0800

    remove app.js, andd test.js

--- test.js
+++ test.js
@@ -1,3 +1,22 @@
 const { FunctionPipeline, OnError } = require('./index');
 
-console.log(FunctionPipeline, OnError)
+(async () => {
+
+    const resolveFn = async (cookie, workspaceId) => {
+        return new Promise(async (resolve, reject) => await setTimeout(resolve, 5000, {fre:'foo'}));
+    }
+
+    const rejectFn = async (cookie) => {
+        return new Promise(async (resolve, reject) => await setTimeout(reject, 5000, 'foo'));
+    }
+
+    const fn1_2 = () => { return 'step-1_2'; }
+
+    const fn2 = () => { return 'step-2'; }
+    
+
+    let pipeline = new FunctionPipeline()
+    await pipeline.add(OnError.CONTINUE, rejectFn, resolveFn, fn1_2).add(OnError.CONTINUE, fn2, rejectFn).perform()
+    console.log('here')
+    console.log(pipeline.execTime)
+})()
